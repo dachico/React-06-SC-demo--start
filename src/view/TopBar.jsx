1edf67f0b8b844ddbdd0381e02746a84f90af7c7
Dachi <151663998+dachico@users.noreply.github.com>
2024-03-25 20:00:28 +0200

    first commit

--- src/view/TopBar.jsx
+++ src/view/TopBar.jsx
@@ -1,9 +1,80 @@
 import React from "react";
+import styled, { keyframes } from "styled-components";
 
-const TopBar =  ({ children }) => (
-  <div className="top-bar">
+const TopBar = ({ children }) => (
+  <Div>
     <h1>{children}</h1>
     <img className="logo" src="icons/logo.svg" alt="logo" />
-  </div>
+  </Div>
 );
 export default TopBar;
+
+const spin = styled.keyframes`
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+`;
+
+const Div = styled.div`
+  background: slategray;
+  width: 100%;
+  height: 70px;
+  padding: 20px;
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+
+  h1 {
+    font-size: 3rem;
+    font-weight: normal;
+    font-family: "Griffy", cursive;
+    color: papayawhip;
+  }
+
+  img {
+    height: 5rem;
+    animation: ${spin} 2s linear infinite;
+  }
+`;
+
+/*
+.top-bar {
+  background: slategray;
+  width: 100%;
+  height: 70px;
+  padding: 20px;
+  color: white;
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.top-bar h1 {
+  font-size: 3rem;
+  font-weight: normal;
+  font-family: "Griffy", cursive;
+  color: papayawhip;
+}
+.logo {
+  height: 5rem;
+  animation: spin 2s linear infinite;
+}
+@keyframes spin {
+  from {
+    transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+  }
+}
+
+*/
